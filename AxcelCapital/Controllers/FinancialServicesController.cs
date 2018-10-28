eac13b92361628ee8a8eab00cf3cde2057bbda3d
JoelFong85 <36076912+JoelFong85@users.noreply.github.com>
2018-10-28 15:49:35 +0800

    first commit

---
+++ AxcelCapital/Controllers/FinancialServicesController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AxcelCapital.Controllers
+{
+    public class FinancialServicesController : Controller
+    {
+        // GET: FinancialServices
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult LoansWithAxcelCapital()
+        {
+            return View();
+        }
+
+        public ActionResult LoansAndFinancialConsultancy()
+        {
+            return View();
+        }
+
+        public ActionResult InvoiceFinancing()
+        {
+            return View();
+        }
+
+
+        public ActionResult BusinessFinancing()
+        {
+            return View();
+        }
+
+
+        public ActionResult PIC()
+        {
+            return View();
+        }
+
+        public ActionResult GovtAssistedSchemes()
+        {
+            return View();
+        }
+
+        public ActionResult PersonalLoanSolutions()
+        {
+            return View();
+        }
+
+        public ActionResult CustomSolutions()
+        {
+            return View();
+        }
+    }
+}
---
+++ AxcelCapital/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AxcelCapital")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AxcelCapital")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1cdede70-1c5a-421d-bae8-a19c24f9c3c8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
