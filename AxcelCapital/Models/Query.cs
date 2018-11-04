3c1ae9c8d3103ce29576d4ed51734d06b9e94577
JoelFong85 <36076912+JoelFong85@users.noreply.github.com>
2018-11-04 14:29:01 +0800

    halfway done with responsive web

---
+++ AxcelCapital/Models/Query.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AxcelCapital.Models
+{
+    public class Query
+    {
+        [Required]
+        [StringLength(50)]
+        public string firstName { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string lastName { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string email { get; set; }
+        [Required]
+        [StringLength(11)]
+        public string mobile { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string companyName{ get; set; }
+        public string turnOver { get; set; }
+        public string message { get; set; }
+    }
+}
