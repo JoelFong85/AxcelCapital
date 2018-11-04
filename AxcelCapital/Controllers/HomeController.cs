fce9af0a786d70bfbb9d5422c15455d468fbdadb
JoelFong85 <36076912+JoelFong85@users.noreply.github.com>
2018-11-04 22:58:57 +0800

    pushing updates as of 4 Nov

--- AxcelCapital/Controllers/HomeController.cs
+++ AxcelCapital/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
+using AxcelCapital.Models;
 
 namespace AxcelCapital.Controllers
 {
@@ -11,6 +13,27 @@ namespace AxcelCapital.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult SubmitQuery(Query query)
+        {
+            //subjectTitle
+            string subjectTitle = "You have a query from " + query.firstName + " " + query.lastName;
+
+            //emailBody
+
+            string emailBody = "Name: " + query.firstName + " " + query.lastName + "<br />" +
+                                "Email: " + query.email + "<br />" +
+                                "Mobile: " + query.mobile + "<br />" + 
+                                "Company Name: " + query.companyName + "<br />" +
+                                "Company Turnover: " + query.turnOver + "<br />" +
+                                "Message: " + query.message ; 
+                
+
+            sendMail(subjectTitle, emailBody);
+
+            return RedirectToAction("Index", "Home");
         }
 
         public ActionResult About()
@@ -32,5 +55,39 @@ namespace AxcelCapital.Controllers
             ViewBag.Message = "Careers Page";
             return View();
         }
+
+        public static void sendMail(string subjectTitle, string emailBody)
+        {
+            int tryAgain = 5;
+            bool failed = true;
+
+            while (failed && tryAgain > 0)
+            {
+                failed = false;
+                try
+                {
+                    SmtpClient smtpClient = new SmtpClient("smtp.gmail.com");
+                    smtpClient.Port = 587;
+                    smtpClient.Credentials = new System.Net.NetworkCredential("joelfong@gmail.com", "fonganama");
+                    smtpClient.EnableSsl = true;
+
+                    MailMessage mail = new MailMessage();
+                    mail.From = new MailAddress("joelfong@gmail.com");
+                    mail.To.Add("joelfong@gmail.com");
+                    mail.Subject = subjectTitle;
+                    mail.IsBodyHtml = true;
+                    mail.Body = emailBody;
+                                       
+                    smtpClient.Send(mail);
+                }
+                catch (Exception ex)
+                {
+                    failed = true;
+                    tryAgain--;
+                }
+            }
+
+        }
     }
+
 }
